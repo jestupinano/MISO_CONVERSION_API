4d60813ce0356e8145b6abdb5bdbe70714e0e328
Andres Martinez <af.martinezc1@uniandes.udu.co>
2023-10-17 21:10:47 -0500

    renombre de variables

--- src/views.py
+++ src/views.py
@@ -13,27 +13,27 @@ class VistaSolicitud(Resource):
         if file_id is None:
             return {'message': 'Debe enviar un id de archivo para descargar'}, 400
         db_request = Solicitudes.query.filter(Solicitudes.id == file_id).first()
-        directorio_destino = f"{db_request.output_path}/{db_request.fileName}"
-        return send_file(directorio_destino, as_attachment=True)
+        destination_path = f"{db_request.output_path}/{db_request.fileName}"
+        return send_file(destination_path, as_attachment=True)
         
     def post(self):
         usuario = request.form['user']
-        archivo = request.files['file']
+        file = request.files['file']
         if usuario is None:
             return {'message':'Debe enviar un usuario al cual asociar la solicitud'}, 400
-        if archivo is None:
+        if file is None:
             return {'message':'Debe enviar un archivo para convertir'}, 400
         
-        filename = secure_filename(archivo.filename)
-        directorio_destino = f"{current_app.config['UPLOAD_FOLDER']}{usuario}"
-        if not os.path.exists(directorio_destino):
-            os.makedirs(directorio_destino)
-        archivo.save(os.path.join(directorio_destino, filename))
+        filename = secure_filename(file.filename)
+        destination_path = f"{current_app.config['UPLOAD_FOLDER']}{usuario}"
+        if not os.path.exists(destination_path):
+            os.makedirs(destination_path)
+        file.save(os.path.join(destination_path, filename))
 
         new_request = Solicitudes(
             user=usuario,
-            input_path=directorio_destino,
-            output_path=directorio_destino,
+            input_path=destination_path,
+            output_path=destination_path,
             fileName=filename,
             status='sendToServer'
         )
